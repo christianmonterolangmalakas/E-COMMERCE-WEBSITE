1ea8b1b28543722a35010fe4f610f30ed95d1e3c
christian montero <christianopogito123@gmail.com>
2024-01-19 15:31:20 +0800

    create MainCarousel.jsx and i import all images from assets folder

---
+++ frontend/src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { shades } from "../../theme";
+const importAll = (r) =>
+  r.keys().reduce((acc, item) => {
+    acc[item.replace("./", "")] = r(item);
+    return acc;
+  }, {});
+
+export const heroTextureImports = importAll(
+  require.context("../../assets", false, /\.(png|jpe?g|svg)$/)
+);
