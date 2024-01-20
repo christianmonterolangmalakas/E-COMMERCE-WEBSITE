1c31bd1cdca3521d9546c9a52c1145a234ec8726
JohnAlbert29 <johnalbert.retiza@tup.edu.ph>
2024-01-20 13:46:48 +0800

    Update Shipping.jsx
    Fix error, adjust shiping form and billing form

--- frontend/src/scenes/checkout/Shipping.jsx
+++ frontend/src/scenes/checkout/Shipping.jsx
@@ -9,25 +9,8 @@ const Shipping = ({
   handleBlur,
   setFieldValue,
 }) => {
-
- {/* SHIPPING FORM */}
-      {!values.shippingAddress.isSameAddress && (
-        <Box>
-          <Typography sx={{ mb: "15px" }} fontSize="18px">
-            Shipping Information
-          </Typography>
-          <AddressForm
-            type="shippingAddress"
-            values={values.shippingAddress}
-            touched={touched}
-            errors={errors}
-            handleBlur={handleBlur}
-            handleChange={handleChange}
-          />
-        </Box>
-      )}
-    </Box >
-         <Box m="30px auto">
+  return (
+    <Box m="30px auto">
       {/* BILLING FORM */}
       <Box>
         <Typography sx={{ mb: "15px" }} fontSize="18px">
@@ -61,8 +44,24 @@ const Shipping = ({
         />
       </Box>
 
-                </Box>
+      {/* SHIPPING FORM */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+          <Typography sx={{ mb: "15px" }} fontSize="18px">
+            Shipping Information
+          </Typography>
+          <AddressForm
+            type="shippingAddress"
+            values={values.shippingAddress}
+            touched={touched}
+            errors={errors}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
-        export default Shipping;
+export default Shipping;
