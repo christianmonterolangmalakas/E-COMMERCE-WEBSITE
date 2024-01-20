64ec917447f099324c96ea4e57f5fb434ec2a1bf
JohnAlbert29 <johnalbert.retiza@tup.edu.ph>
2024-01-20 13:28:28 +0800

    Update shipping.jsx
    Create billing form

--- frontend/src/scenes/checkout/Shipping.jsx
+++ frontend/src/scenes/checkout/Shipping.jsx
@@ -18,5 +18,38 @@ import AddressForm from "./AddressForm";
         </Box>
       )}
     </Box >
+         <Box m="30px auto">
+      {/* BILLING FORM */}
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Billing Information
+        </Typography>
+        <AddressForm
+          type="billingAddress"
+          values={values.billingAddress}
+          touched={touched}
+          errors={errors}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+      </Box>
+
+      <Box mb="20px">
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+          label="Same for Shipping Address"
+        />
+      </Box>
         
         export default Shipping;
